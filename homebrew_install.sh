40b1411699ba7553f55251010f37012d2870309c
Yael Goldin <ygoldin@cs.washington.edu>
2019-08-15 22:28:23 -0700

    create script for homebrew and its packages

---
+++ homebrew_install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew cask install android-platform-tools
+brew cask install clion
+brew cask install firefox
+brew cask install google-chrome
+brew cask install intellij-idea
+brew cask install java
+brew cask install pycharm
+brew cask install spectacle
+brew cask install spotify
+brew cask install telegram
+brew cask install visual-studio-code
+
+brew install cmake
+brew install python
+brew install scrcpy
+brew install tmux
